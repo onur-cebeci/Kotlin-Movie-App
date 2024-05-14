de2af63d1a02c8bdfd184107dc595da78bac3f26
Onesinus SPT <onur.cebeci@egegen.com>
2024-05-14 15:48:06 +0300

    home screen design has been complated

--- app/src/main/java/com/onurcebeci/movieapp/MainActivity.kt
+++ app/src/main/java/com/onurcebeci/movieapp/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.onurcebeci.movieapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.onurcebeci.movieapp.navigation.BannerScreen
 import com.onurcebeci.movieapp.navigation.Navigation
@@ -18,19 +26,39 @@ import com.onurcebeci.movieapp.ui.theme.MovieAppTheme
 import com.onurcebeci.movieapp.viewModel.MovieViewModel
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
             MovieAppTheme {
-                // A surface container using the 'background' color from the theme
+                val linearGradientBrush = Brush.linearGradient(
+                    colors = listOf(
+
+                        Color(0xFF5995EE),
+                        Color(0xFF3B5FB4),
+                        Color(0xFF31394B),
+
+                        Color(0xFF3D3535)
+                    ),
+                    start = Offset(Float.POSITIVE_INFINITY, 0f),
+                    end = Offset(0f, Float.POSITIVE_INFINITY),
+                )
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
 
                     val movieViewModel = viewModel<MovieViewModel>()
                     val state = movieViewModel.state
 
-                 //   Text(text = state.movies.toString())
+            Box(modifier= Modifier.fillMaxSize().background(linearGradientBrush)){
+                Navigation()
 
-                            Navigation()
+            }
+
+
 
                 }
             }
